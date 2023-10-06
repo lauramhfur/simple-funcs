4118994f27f714e6e9dabe56cf0df2f0409d3812
Laura <lauramhfur@gmail.com>
2023-10-06 15:50:33 +0200

    CTiB Exercises Week 6

--- src/funcs.py
+++ src/funcs.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 """Exercises with simple functions"""
 
 
@@ -6,19 +8,21 @@ def prod(a, b, c):
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    6
     """
-    ...
+    return a*b*c
 
+a = 1
 
 def prod2(b):
     """
     Get a global a and write to a local c before computing prod(a, b, c)
 
     >>> prod2(42)
-    'TEST ME'
+    84
     """
-    ...
+    c = 2
+    return a*b*c
 
 
 def longest(x, y):
@@ -26,9 +30,9 @@ def longest(x, y):
     Returning the longest of two lists.
 
     >>> longest([1, 2, 3], [4, 5])
-    'TEST ME'
+    [1, 2, 3]
     """
-    ...
+    return x if len(x) >= len(y) else y
 
 
 def dist(p1, p2):
@@ -36,8 +40,8 @@ def dist(p1, p2):
     Compute the distance between p1 and p2.
 
     >>> dist((1,2), (3,4))
-    'TEST ME'
+    2.83
     """
     x1, y1 = p1
     x2, y2 = p2
-    ...
+    return round(sqrt((x1-x2)**2+(y1-y2)**2), 2)
